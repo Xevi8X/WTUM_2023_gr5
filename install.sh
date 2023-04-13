61905aa6d81306294304a6d55ad0e301ab210c48
Birchman <jakubbrzoskowski@gmail.com>
2023-04-13 18:28:45 +0200

    Add installation and launching scripts

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pip install -r requirements.txt
+git clone https://github.com/WongKinYiu/yolov7.git
+mkdir models
+echo "Insert models from https://github.com/Xevi8X/WTUM_2023_gr5/releases/tag/23.03.2023 into ./models folder"
